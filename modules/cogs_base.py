930e545c4f625e7413d653324688fed4e984171e
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-04-08 19:12:20 +0530

    completed the base cog

---
+++ modules/cogs_base.py
@@ -0,0 +1,28 @@
+import platform
+
+import discord
+from discord.ext import commands
+
+
+class Base(commands.Cog, description="Main stuff related to the bot"):
+
+    def __init__(self, client: commands.Bot):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f'[*] Python Version: {platform.python_version()}')
+        print(f'[*] Discord.py API Version: {discord.__version__}')
+        print(f'[*] Logged in as {self.client.user} | {self.client.user.id}')
+
+        await self.client.change_presence(
+            activity=discord.Activity(
+                type=discord.ActivityType.watching,
+                name=f'{len(self.client.guilds)} servers!'
+            )
+        )
+        print(f'[*] Bot is ready to be used!')
+
+
+def setup(client: commands.Bot):
+    client.add_cog(Base(client))
