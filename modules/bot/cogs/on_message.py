b500741021d1d3c79123e2258cfcd7188c2a4ac8
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-04-09 08:27:23 +0530

    Update on_message.py

--- modules/bot/cogs/on_message.py
+++ modules/bot/cogs/on_message.py
@@ -1,17 +1,26 @@
 import discord
 from discord.ext import commands
-from datetime import datetime
+
+from bot.database.manager import blacklisted
+from bot.database.manager import main
 
 
-class ErrorHandling(commands.Cog, description="Handle the errors raised by the bot"):
+class OnMessage(commands.Cog, description="Handle the errors raised by the bot"):
 
     def __init__(self, client: commands.Bot):
         self.client = client
 
     @commands.Cog.listener()
     async def on_command_error(self, message):
-        pass
+
+        if self.client.user == message.author:
+            return
+
+        if message.author.id in blacklisted.blacklisted_users_all_lines:
+            if str(message.content).startswith(main.prefix):
+                await message.reply('You are blacklisted from usin this bot!')
+                return
 
 
 def setup(client: commands.Bot):
-    client.add_cog(ErrorHandling(client))
+    client.add_cog(OnMessage(client))
