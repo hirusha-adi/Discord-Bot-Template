a36bd672e0431afa5d6865b767c312457c2e6c4a
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-04-09 15:17:56 +0530

    load and unload cogs

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from bot.database.manager import main as db_main
 client = commands.Bot(command_prefix=db_main.prefix)
 
 for filename in os.listdir('./bot/cogs'):
-    if filename.endswith('.py'):  # cogs.musicplayer is not being loded in here
+    if filename.endswith('.py'):
         try:
             client.load_extension(f'bot.cogs.{filename[:-3]}')
             print(f"[+] Loaded: bot.cogs.{filename[:-3]}")
@@ -17,4 +17,24 @@ for filename in os.listdir('./bot/cogs'):
                 f"[+] Unable to load: bot.cogs.{filename[:-3]}  :  {excl}")
 
 
-# client.run(db_main.token)
+@client.command()
+async def loadex(ctx, extension):
+    if ctx.author.id in db_main.admins:
+        client.load_extension(
+            f'bot.cogs.{extension if not(str(extension).endswith(".py")) else extension[:-3]}')
+        await ctx.send(f"Loaded cog: {extension}")
+    else:
+        await ctx.send("You do not have permissions to use this command!")
+
+
+@client.command()
+async def unloadex(ctx, extension):
+    if ctx.author.id in db_main.admins:
+        client.unload_extension(
+            f'bot.cogs.{extension if not(str(extension).endswith(".py")) else extension[:-3]}')
+        await ctx.send(f"Un-Loaded cog: {extension}")
+    else:
+        await ctx.send("You do not have permissions to use this command!")
+
+
+client.run(db_main.token)
