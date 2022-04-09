d8c0055260e3b01610d04bfce3a03bebd7b66a98
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-04-09 08:21:00 +0530

    Update blacklisted.py

---
+++ modules/bot/database/manager/blacklisted.py
@@ -0,0 +1,19 @@
+from bot.utils.filenames import database
+
+with open(database.blacklisted_users, "r", encoding="utf-8") as _file:
+    blacklisted_users_all_lines_first = _file.readlines()
+    blacklisted_users_all_lines = blacklisted_users_all_lines_first
+
+
+def addUser(user_id):
+    if not(user_id in blacklisted_users_all_lines):
+        blacklisted_users_all_lines.append(user_id)
+        _updateListFile()
+
+
+def _updateListFile():
+    if not(len(blacklisted_users_all_lines_first) == len(blacklisted_users_all_lines)):
+        blacklisted_users_all_lines_first = blacklisted_users_all_lines
+        with open(database.blacklisted_users, "w", encoding="utf-8") as _file:
+            for line in blacklisted_users_all_lines:
+                _file.write(str(line))
