0a3eb31d458d5053f84f7000a42257e0fa4e5c5d
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-04-09 09:15:17 +0530

    blacklist users

--- modules/bot/cogs/base.py
+++ modules/bot/cogs/base.py
@@ -3,6 +3,9 @@ import platform
 import discord
 from discord.ext import commands
 
+from bot.database.manager.blacklisted import addGuild, addUser, addChannel
+from bot.database.manager.blacklisted import blacklisted_channels_all_lines, blacklisted_guilds_all_lines, blacklisted_users_all_lines
+
 
 class Base(commands.Cog, description="Main stuff related to the bot"):
 
@@ -22,6 +25,30 @@ class Base(commands.Cog, description="Main stuff related to the bot"):
             )
         )
         print(f'[*] Bot is ready to be used!')
+
+    @commands.command()
+    async def blacklist(self, ctx, sub_cat, id):
+        user_list = ("user", "u")
+        channel_list = ("channel", "ch", "c")
+        guild_list = ("guild", "g", "server", "s")
+
+        if sub_cat in user_list:
+            addUser(str(id))
+            await ctx.send(f'Added {id} to the blacklisted users list')
+            await ctx.send(f'New length of blacklisted users list: {len(blacklisted_users_all_lines)}')
+
+        elif sub_cat in channel_list:
+            addChannel(str(id))
+            await ctx.send(f'Added {id} to the blacklisted channels list')
+            await ctx.send(f'New length of blacklisted channels list: {len(blacklisted_channels_all_lines)}')
+
+        elif sub_cat in guild_list:
+            addGuild(str(id))
+            await ctx.send(f'Added {id} to the blacklisted guild list')
+            await ctx.send(f'New length of blacklisted guild list: {len(blacklisted_guilds_all_lines)}')
+
+        else:
+            await ctx.send('Please mention a category: ( user, channel, guild )')
 
 
 def setup(client: commands.Bot):
